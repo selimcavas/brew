3912f67469f7f4b0cfdc3c547fac75d44f51c4a1
Selim Cavas <selim.cavas@ozu.edu.tr>
2023-05-07 23:53:08 +0300

    added card file
    Co-authored-by: MertAdak <MertAdak@mert.adak@ozu.edu.tr>
    
    Co-authored-by: berkadak <berkadak@berk.adak@ozu.edu.tr>

---
+++ brew/src/components/Card.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../styles/Card.css';
+
+function Card(props) {
+  return (
+    <div className="card">
+      <img
+        src={props.imagePath}
+        alt={props.imageAlt}
+        width="300"
+        height="300"
+      />
+      <div className="card-body">
+        <h5 className="card-title">{props.title}</h5>
+        <p className="card-subtitle">{props.subtitle}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
