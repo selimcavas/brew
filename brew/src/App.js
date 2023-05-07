9adc8877c9dc9d5c2c13e8c8da2482628b1a252f
Selim Cavas <selim.cavas@ozu.edu.tr>
2023-05-07 23:51:24 +0300

    modified index and app

--- brew/src/App.js
+++ brew/src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
+import './styles/App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Main from './pages/Main.js';
+
+// import ReactDOM from 'react-dom/client';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Main from './pages/Main';
+// import Order from './pages/Order';
+// import MyOrder from './pages/MyOrder';
+// import Contact from './pages/Contact';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
-  );
+  return <Main />;
 }
-
 export default App;
