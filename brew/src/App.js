cc210aed4b6c6ead1a9d61a369dee68c59fc14b0
Selim Cavas <selim.cavas@ozu.edu.tr>
2023-05-09 22:44:11 +0300

    added routes to App
    Co-authored-by: MertAdak <mert.adak@ozu.edu.tr>
    Co-authored-by: berkadak <berk.adak@ozu.edu.tr>

--- brew/src/App.js
+++ brew/src/App.js
@@ -1,15 +1,23 @@
 import './styles/App.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import Main from './pages/Main.js';
-
-// import ReactDOM from 'react-dom/client';
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import Main from './pages/Main';
-// import Order from './pages/Order';
-// import MyOrder from './pages/MyOrder';
-// import Contact from './pages/Contact';
+import Main from './pages/Main';
+import Order from './pages/Order';
+import MyOrder from './pages/MyOrder';
+import Contact from './pages/Contact';
+import NavBar from './components/NavBar';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <Main />;
+  return (
+    <div className="App">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/order" element={<Order />} />
+        <Route path="/my-order" element={<MyOrder />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
 }
 export default App;
