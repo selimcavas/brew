0f1181abf95718e6384ec187210e2d1ad95fe91e
Selim Cavas <selim.cavas@ozu.edu.tr>
2023-05-09 22:46:03 +0300

    updated index for routes
    Co-authored-by: MertAdak <mert.adak@ozu.edu.tr>
    Co-authored-by: berkadak <berk.adak@ozu.edu.tr>

--- brew/src/index.js
+++ brew/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
 import './styles/index.css';
 import App from './App';
+import { BrowserRouter, BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
