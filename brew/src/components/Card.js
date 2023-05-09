633e449cecf0574c215b2db4bc6c74056d460b7f
Selim Cavas <selim.cavas@ozu.edu.tr>
2023-05-09 14:25:16 +0300

    updated card

--- brew/src/components/Card.js
+++ brew/src/components/Card.js
@@ -12,7 +12,7 @@ function Card(props) {
       />
       <div className="card-body">
         <h5 className="card-title">{props.title}</h5>
-        <p className="card-subtitle mt-2">{props.subtitle}</p>
+        <p className="card-subtitle mt-3">{props.subtitle}</p>
       </div>
     </div>
   );
