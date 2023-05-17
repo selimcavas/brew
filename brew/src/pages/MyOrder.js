68092eae8804be8618b47d6e957b4357e7712698
Selim Cavas <selim.cavas@ozu.edu.tr>
2023-05-18 02:24:19 +0300

    added OrderCards from backend
    Co-authored-by: MertAdak <mert.adak@ozu.edu.tr>
    Co-authored-by: berkadak <berk.adak@ozu.edu.tr>

--- brew/src/pages/MyOrder.js
+++ brew/src/pages/MyOrder.js
@@ -1,33 +1,10 @@
 import React from 'react';
-import OrderCard from '../components/MyOrderCard';
+import { OrderCards } from '../backend.js';
 
 function MyOrder() {
   return (
     <div id="myOrders">
-      <OrderCard
-        coffeeType="americano"
-        name="Classic Americano"
-        price="4.99"
-        time="1 hour ago"
-      />
-      <OrderCard
-        coffeeType="latte"
-        name="Latte"
-        price="4.99"
-        time="1 hour ago"
-      />
-      <OrderCard
-        coffeeType="frappe"
-        name="Frappe"
-        price="4.99"
-        time="1 hour ago"
-      />
-      <OrderCard
-        coffeeType="mocha"
-        name="Mocha"
-        price="4.99"
-        time="1 hour ago"
-      />
+      <OrderCards />
     </div>
   );
 }
