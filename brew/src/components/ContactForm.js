e8c383c638a4978a749537eb8323773e59e91bf4
Selim Cavas <selim.cavas@ozu.edu.tr>
2023-05-18 02:22:54 +0300

    modified contact form
    Co-authored-by: MertAdak <mert.adak@ozu.edu.tr>
    Co-authored-by: berkadak <berk.adak@ozu.edu.tr>

--- brew/src/components/ContactForm.js
+++ brew/src/components/ContactForm.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import '../styles/ContactForm.css';
+import { ContactFunc } from '../backend.js';
 
 function ContactForm() {
   return (
     <div id="contactDiv">
-      <form id="ContactForm">
+      <form id="ContactForm" onSubmit={ContactFunc}>
         <br />
         <header>CONTACT FORM</header>
-        <input id="name" type="text" placeholder="NAME" class="formElements" />
+        <input
+          id="name"
+          type="text"
+          placeholder="FULL NAME"
+          class="formElements"
+          required
+        />
         <input
           id="email"
           type="text"
           placeholder="E-MAIL"
           class="formElements"
+          required
         />
         <textarea
           id="message"
           type="text"
           placeholder="MESSAGE"
           class="formElements"
+          required
         ></textarea>
         <input id="submit" type="submit" value="GO!" class="formElements" />
       </form>
