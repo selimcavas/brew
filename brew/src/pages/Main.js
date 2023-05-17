5437574872990eac101d8a41dbcf4adc21842695
Selim Cavas <selim.cavas@ozu.edu.tr>
2023-05-18 02:24:35 +0300

    added prices
    Co-authored-by: MertAdak <mert.adak@ozu.edu.tr>
    Co-authored-by: berkadak <berk.adak@ozu.edu.tr>

--- brew/src/pages/Main.js
+++ brew/src/pages/Main.js
@@ -16,6 +16,7 @@ function Main() {
             imageAlt="Latte"
             title="Latte"
             subtitle=" Smooth and velvety espresso combined with creamy steamed milk for a perfect balance of boldness and sweetness."
+            price="4.5$"
           />
           <br></br>
           <Card
@@ -23,6 +24,7 @@ function Main() {
             imageAlt="Mocha"
             title="Mocha"
             subtitle="Decadent espresso and chocolate come together in a rich and indulgent drink, topped with whipped cream for the ultimate treat."
+            price="5.0$"
           />
         </div>
         <div class="col-lg-6 col-md-12">
@@ -31,6 +33,7 @@ function Main() {
             imageAlt="Americano"
             title="Americano"
             subtitle="Strong and bold espresso shots diluted with ice for a smooth and refreshing coffee experience, perfect for hot weathers"
+            price="4.0$"
           />
           <br></br>
           <Card
@@ -38,6 +41,7 @@ function Main() {
             imageAlt="Frappe"
             title="Frappe"
             subtitle="A refreshing blend of espresso, milk, and ice, finished with a dollop of whipped cream and a drizzle of caramel or chocolate sauce."
+            price="6.0$"
           />
         </div>
       </div>
